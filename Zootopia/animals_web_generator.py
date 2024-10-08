802a4eb2d01e3ba94a0ecd0d92895678aa5e58fe
david <davidunger@outlook.com>
2024-10-08 20:37:11 +0200

    Added Logic to generate html template and exceptions

--- Zootopia/animals_web_generator.py
+++ Zootopia/animals_web_generator.py
@@ -1,36 +1,68 @@
 import json
 import os
 
-
-
-json_file_path = os.path.join('Zootopia', 'animals_data.json')
+def main():
+    json_file_path = os.path.join('Zootopia', 'animals_data.json')
+    html_file_path = os.path.join('Zootopia', 'animals_template.html')
+    animals_data = load_data(json_file_path)
+    dog_string = string_animals(animals_data)
+    html_template = load_html(html_file_path)
+    new_html_template = html_template.replace("__REPLACE_ANIMALS_INFO__",dog_string)
+    new_html_file_path = os.path.join('Zootopia', 'new_animals_template.html')
+    write_html(new_html_template,new_html_file_path)
+    
 
 
 def load_data(file_path):
-    with open(file_path, "r") as handle:
-        return json.load(handle)
+    ###loading json file from parameter filepath and returing dictionary###
+    try:
+        with open(file_path, "r") as handle:
+            return json.load(handle)
+    except FileNotFoundError as e:
+        print(e) 
+
+def string_animals(data):
+    ###Creating a string from Dict parameter for animals returning string outpu###
+    output =""
+    try:
+        for animal in data:
+            name_dog = animal["name"]
+            diet_dog = animal["characteristics"]["diet"]
+            location_dog = animal["locations"][0]
+            
+            if "characteristics" in animal and "type" in animal["characteristics"]:
+                type_dog = animal["characteristics"]["type"]
+            else:
+                type_dog = ""
+            output += f"Name: {name_dog}\n"
+            output += f"Diet: {diet_dog}\n"
+            output += f"Location: {location_dog}\n"
+            if len(type_dog) > 0:
+                output += f"Type: {type_dog}\n"
+            output += "\n"    
+        return output 
+    except Exception as e:
+        print(e)           
 
 
-animals_data = load_data(json_file_path)
+def load_html(file_path):
+    ###Loading html template as parameter and returning it as string###
+    try:
+        with open(file_path, "r") as dog_data:
+            template_content = dog_data.read()
+        return template_content
+    except FileNotFoundError as e:
+        print(e)
+
+
+def write_html(template,new_html_file_path):
+    try:
+        with open(new_html_file_path,"w", encoding="UTF8") as html_file:
+            html_file.write(template)
+    except IOError as e:
+        print(e)        
 
 
 
-def print_animals(data):
-    for animal in data:
-        name_dog = animal["name"]
-        diet_dog = animal["characteristics"]["diet"]
-        location_dog = animal["locations"][0]
-        
-        if "characteristics" in animal and "type" in animal["characteristics"]:
-            type_dog = animal["characteristics"]["type"]
-        else:
-            type_dog = ""
-        
-        print(f"Name: {name_dog}")
-        print(f"Diet: {diet_dog}")
-        print(f"Location: {location_dog}")
-        if len(type_dog) >0:
-            print(f"Type: {type_dog}")
-        print()
-
-print_animals(animals_data)
+if __name__ == "__main__":
+    main()
